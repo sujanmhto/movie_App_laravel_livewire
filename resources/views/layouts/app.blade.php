fe1f31aff964062cb26c3ee94116e3e8b0fc82e2
sujanmhto <sujanmhto@gmail.com>
2022-01-31 20:29:37 +0545

    Models Imported with Migration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,22 +21,27 @@
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
+        <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
+          <x-sidebar/>
+          <div class="flex flex-col flex-1 overflow-hidden">
+            <x-header/>
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
             @endif
 
             <!-- Page Content -->
-            <main>
+            <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+                <div class="container px-6 py-8 mx-auto">
+
                 {{ $slot }}
+                </div>
             </main>
+        </div>
         </div>
 
         @stack('modals')
